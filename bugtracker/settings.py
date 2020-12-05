391d381dd391204c2a838cfefb924c46cdb61ef6
thomasplantin <tplantin@utexas.edu>
2020-12-05 12:55:32 -0600

    Images now stored in AWS S3

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,14 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER=config('EMAIL_USER')
 EMAIL_HOST_PASSWORD=config('EMAIL_PASS')
+
+AWS_STORAGE_BUCKET_NAME=config('AWS_S3_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = config('AWS_S3_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_S3_SECRET_ACCESS_KEY')
+
+AWS_S3_FILE_OVERWRITE = False
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
